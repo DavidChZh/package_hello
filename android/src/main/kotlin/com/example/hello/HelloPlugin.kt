6c8de468e1eb3e1c7259b426a05eeb49f1cea13f
zhangchong <sazjc@sina.com>
2024-10-22 09:30:48 +0800

    1

--- android/src/main/kotlin/com/example/hello/HelloPlugin.kt
+++ android/src/main/kotlin/com/example/hello/HelloPlugin.kt
@@ -23,7 +23,7 @@ class HelloPlugin: FlutterPlugin, MethodCallHandler {
 
   override fun onMethodCall(call: MethodCall, result: Result) {
     if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+      result.success("Android122 ${android.os.Build.VERSION.RELEASE}")
     } else {
       result.notImplemented()
     }
